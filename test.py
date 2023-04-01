ec6946fb737f22a8103b3dccbf5c8109abf6a156
sharmarajesh <sharmarajesh27@gmail.com>
2023-04-01 23:15:31 +0530

    panda

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-import random
-
-print(random.randint(0,9))
+import pandas as pd  
+      
+# List1  
+lst = [['Geek', 25], ['is', 30], 
+                     ['for', 26], ['Geeksforgeeks', 22]] 
+        
+# creating df object with columns specified    
+df = pd.DataFrame(lst, columns =['Tag', 'number']) 
+print(df )
